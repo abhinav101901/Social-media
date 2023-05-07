6905353a318327ce3aeca4985b82a19c6b8b908f
abhinav101901 <abhinav8299233838@gmail.com>
2023-05-07 10:35:56 +0530

    userController

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -9,30 +9,34 @@ const bcrypt = require('bcrypt');
 const createUser = async (req, res) => {
     try {
         if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "please provide information" });//[]
-        let { fname, lname, email, phone, password,...a } = req.body;
-        if(Object.keys(a).length!=0) return res.status(400).send({status:false,message:`please remove ${Object.keys(a)}`})
+        let { fullname, username, gender, email, phone, password, ...a } = req.body;
+        if (Object.keys(a).length != 0) return res.status(400).send({ status: false, message: `please remove ${Object.keys(a)}` })
 
-        if (!isValidString(fname)) return res.status(400).send({ status: false, message: "fname is mandatory, and should be in string format" })
-        if (!isValidName(fname)) { return res.status(400).send({ status: false, message: 'fname should be in Alphabets' }) }
+        if (!isValidString(fullname)) return res.status(400).send({ status: false, message: "fullname is mandatory, and should be in string format" })
+        if (!isValidName(fullname)) { return res.status(400).send({ status: false, message: 'fullname should be in Alphabets' }) }
 
-        if (!isValidString(lname)) return res.status(400).send({ status: false, message: "lname is mandatory, and should be in string format" })
-        if (!isValidName(lname)) return res.status(400).send({ status: false, message: 'lname should be in Alphabets'})
+        if (!isValidString(username)) return res.status(400).send({ status: false, message: "username is mandatory, and should be in string format" })
+        // if (!isValidName(username)) return res.status(400).send({ status: false, message: 'username should be in Alphabets'})
+     
+        if (!isValidString(gender)) return res.status(400).send({status:false, message: "Gender is mandatory, and should be in string format"})
+        if(!["Male","Female","Other"].includes(gender))return res.status(400).send({status:false, message: "Only Enter Male,female and Other"})
 
         if (!isValidString(email)) return res.status(400).send({ status: false, message: "email is mandatory, and should be in string format" })
-        if (!isValidEmail(email)) return res.status(400).send({ status: false, message: 'Please enter valid emailId' }) 
+        if (!isValidEmail(email)) return res.status(400).send({ status: false, message: 'Please enter valid emailId' })
 
 
         if (!isValidString(phone)) return res.status(400).send({ status: false, message: "phone is mandatory, and should be in string format" })
-        if (!isValidMobileNumber(phone)) return res.status(400).send({ status: false, message: 'Please enter valid Mobile Number' }) 
+        if (!isValidMobileNumber(phone)) return res.status(400).send({ status: false, message: 'Please enter valid Mobile Number' })
 
         if (!isValidString(password)) return res.status(400).send({ status: false, message: "password is mandatory, and should be in string format" })
-        if (!isValidpassword(password)) return res.status(400).send({ status: false, message: "To make strong Password Should be use 8 to 15 Characters which including letters, atleast one special character and at least one Number." }) 
+        if (!isValidpassword(password)) return res.status(400).send({ status: false, message: "To make strong Password Should be use 8 to 15 Characters which including letters, atleast one special character and at least one Number." })
         req.body.password = await bcrypt.hash(password, 12);
 
-        const isDuplicateEmail = await userModel.findOne({ $or: [{ email: email }, { phone: phone }] })
+        const isDuplicateEmail = await userModel.findOne({ $or: [{ email: email }, { phone: phone }, { username: username }] })
         if (isDuplicateEmail) {
             if (isDuplicateEmail.email == email) { return res.status(400).send({ status: false, message: `This EmailId: ${email} is already exist!` }) }
             if (isDuplicateEmail.phone == phone) { return res.status(400).send({ status: false, message: `This Phone No.: ${phone} is already exist!` }) }
+            if (isDuplicateEmail.username == username) { return res.status(400).send({ status: false, message: `This Username: ${username} is already exist!` }) }
         }
 
         let files = req.files;
@@ -41,7 +45,6 @@ const createUser = async (req, res) => {
 
             if (files.length > 1) return res.status(400).send({ status: false, message: "You can't enter more than one file for Create!" })
             if (!isValidImage(files[0]['originalname'])) { return res.status(400).send({ status: false, message: "You have to put only Image." }) }
-
             console.log(files[0]);
             let imgUrl = await uploadImage(files[0])
             console.log(imgUrl);
@@ -52,7 +55,7 @@ const createUser = async (req, res) => {
         }
 
         let userCreated = await userModel.create(req.body)
-        let {__v,...userDetails}=userCreated._doc
+        let { __v, ...userDetails } = userCreated._doc
         return res.status(201).send({ status: true, message: "User created successfully", data: userDetails })
 
     } catch (err) {
@@ -99,11 +102,11 @@ const getUser = async (req, res) => {
     try {
         let userId = req.params.userId;
         if (!isValidObjectId(userId)) return res.status(400).send({ status: false, message: "invalid userId" })
-//________________________________Authorization_________________________________________________________________________
+        //________________________________Authorization_________________________________________________________________________
         if (userId != req.decodedToken) return res.status(403).send({ status: false, message: "unauthorized" })
-//______________________________________________________________________________________________________________________
-        
-        let userDetails = await userModel.findOne({ _id: userId}).select({ __v: 0 })
+        //______________________________________________________________________________________________________________________
+
+        let userDetails = await userModel.findOne({ _id: userId }).select({ __v: 0 })
         if (!userDetails) return res.status(404).send({ status: false, message: "user Not found" })
 
         return res.status(200).send({ status: true, message: "Successfull", data: userDetails })
@@ -118,29 +121,31 @@ const updateUser = async (req, res) => {
     try {
         let userId = req.params.userId
         if (!isValidObjectId(userId)) return res.status(400).send({ status: false, message: "Please Enter the valid UserId" })
-//________________________________Authorization____________________________________________________________________
+        //________________________________Authorization____________________________________________________________________
 
         if (userId != req.decodedToken) return res.status(403).send({ status: false, message: "unauthorized" })
-//__________________________________________________________________________________________________________________
+        //__________________________________________________________________________________________________________________
 
 
-        let { fname, lname, email, phone, password,...a } = req.body;
-        if(Object.keys(a).length!=0) return res.status(400).send({status:false,message:`please remove ${Object.keys(a)}`})
+        let { fullname, username, email, phone, password, ...a } = req.body;
+        if (Object.keys(a).length != 0) return res.status(400).send({ status: false, message: `please remove ${Object.keys(a)}` })
 
         let final = {}
-        
+
         if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "Please enter some DETAILS!!!" })
 
-        if (fname) {
-            if (!isValidString(fname)) return res.status(400).send({ status: false, message: "Please Enter in String formate First-Name" })
-            if (!/^[a-zA-Z]{3,15}$/.test(fname)) return res.status(400).send({ status: false, message: "Please Enter Valid first-Name" })
-            final.fname = fname
+        if (fullname) {
+            if (!isValidString(fullname)) return res.status(400).send({ status: false, message: "Please Enter in String formate First-Name" })
+            if (!/^[a-zA-Z]{3,15}$/.test(fullname)) return res.status(400).send({ status: false, message: "Please Enter Valid first-Name" })
+            final.fullname = fullname
         }
 
-        if (lname) {
-            if (!isValidString(lname)) return res.status(400).send({ status: false, message: "Please Enter in String formate Last-Name" })
-            if (!/^[a-zA-Z]{3,15}$/.test(lname)) return res.status(400).send({ status: false, message: "Please Enter Valid Last-Name" })
-            final.lname = lname
+        if (username) {
+            if (!isValidString(username)) return res.status(400).send({ status: false, message: "Please Enter in String formate username" })
+            // if (!/^[a-zA-Z]{3,15}$/.test(lname)) return res.status(400).send({ status: false, message: "Please Enter Valid username" })
+            let isUsernameExist = await userModel.findOne({ username: username })
+            if (isUsernameExist) return res.status(400).send({ status: false, message: `This username: ${username} is already exist!` })
+            final.username = username
         }
 
         if (email) {
@@ -166,14 +171,14 @@ const updateUser = async (req, res) => {
             const hashedPassword = await bcrypt.hash(password, 12)
             final.password = hashedPassword
         }
-        
+
         let profileImages = req.files
         if (profileImages && profileImages.length > 0) {
             let url = await uploadImage(profileImages[0])
             final.profileImage = url
         }
 
-        const updatedUser = await userModel.findOneAndUpdate({ _id: userId }, final, { new: true }).select({__v:0})
+        const updatedUser = await userModel.findOneAndUpdate({ _id: userId }, final, { new: true }).select({ __v: 0 })
         if (!updatedUser) return res.status(404).send({ status: false, message: "User does not exist" })
 
         return res.status(200).send({ status: true, message: "Successfully Updated", data: updatedUser })
@@ -184,4 +189,60 @@ const updateUser = async (req, res) => {
     }
 }
 
-module.exports = { createUser, userLogin, getUser, updateUser }
+const follow = async (req, res) => {
+    // make sure the user exists
+    const user = await userModel.findById(req.params.id);
+
+    if (!user) {
+        return res.status(404).send({ status: false, message: `No user found for id ${req.params.id}`});
+    }
+
+    // make the sure the user is not the logged in user
+    if (req.params.id === req.user.id) {
+        return res.status(400).send({ status:false, message: "You can't unfollow/follow yourself"});
+    }
+
+    // only follow if the user is not following already
+    if (user.followers.includes(req.user.id)) {
+        return res.status(400).send({ status:false, message: "You are already following him"});
+    }
+
+    await userModel.findByIdAndUpdate(req.params.id, {
+        $push: { followers: req.user.id },
+        $inc: { followersCount: 1 },
+    });
+    await userModel.findByIdAndUpdate(req.user.id, {
+        $push: { following: req.params.id },
+        $inc: { followingCount: 1 },
+    });
+
+    res.status(200).json({ success: true, data: {} });
+}
+
+
+const unfollow = async (req, res) => {
+    const user = await userModel.findById(req.params.id);
+  
+    if (!user) {
+      return res.status(400).send({ status:false, message: `No user found for ID ${req.params.id}`});
+    }
+  
+    // make the sure the user is not the logged in user
+    if (req.params.id === req.user.id) {
+      return res.status(400).send({ status:false, message: "You can't follow/unfollow yourself"});
+    }
+  
+    await userModel.findByIdAndUpdate(req.params.id, {
+      $pull: { followers: req.user.id },
+      $inc: { followersCount: -1 },
+    });
+    await userModel.findByIdAndUpdate(req.user.id, {
+      $pull: { following: req.params.id },
+      $inc: { followingCount: -1 },
+    });
+  
+    res.status(200).json({ success: true, data: {} });
+}
+  
+
+module.exports = { createUser, userLogin, getUser, updateUser, follow, unfollow}
--- Models/userModel.js
+++ Models/userModel.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
-    fname: {
+    fullname: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
     },
-    lname: {
+    username: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        unique: true,
+    },
+    gender:{
+        type:String,
+        enum:["Male","Female","Other"],
+        required: true
     },
     email: {
         type: String,
@@ -30,8 +36,36 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        trim: true  
-    }
+        trim: true
+    },
+    followers: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+    }],
+    followersCount: {
+        type: Number,
+        default: 0,
+    },
+    followingCount: {
+        type: Number,
+        default: 0,
+    },
+    following: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+    }],
+    posts: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "Post"
+    }],
+    postCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 }, { timestamps: true })
 
 module.exports = mongoose.model('User', userSchema)
